693e928a1dc1fd3583d98713247a37d1b3e92404
srenballe <srenballe@gmail.com>
2017-11-05 12:30:02 +0100

    init

---
+++ Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Opc.Ua;
+
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new Client("opc.tcp://" + Utils.GetHostName() + ":51210/UA/SampleServer");
+        }
+    }
+}
---
+++ OpcUaOpenInterface/opcClient/ServerSubscription.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using Opc.Ua;
+using Opc.Ua.Client;
+
+
+namespace OpcUaOpenInterface
+{
+    public static class ServerSubscription
+    {
+
+        public static async Task Create(Session session, string nodeId, int interval = 5000)
+        {
+            Subscription subscription = new Subscription(session.DefaultSubscription) { PublishingInterval = interval };
+
+            Console.WriteLine("Subscription created");
+            var list = new List<MonitoredItem> {
+               new MonitoredItem(subscription.DefaultItem)
+               {
+                   DisplayName = "ServerStatusCurrentTime", StartNodeId = nodeId
+               }
+           };
+
+            list.ForEach(i => i.Notification += OnNotification);
+            subscription.AddItems(list);
+
+            session.AddSubscription(subscription);
+            subscription.Create();
+
+
+        }
+
+        private static void OnNotification(MonitoredItem item, MonitoredItemNotificationEventArgs e)
+        {
+            foreach (var value in item.DequeueValues())
+            {
+                Console.WriteLine("{0}: {1}, {2}, {3}", item.DisplayName, value.Value, value.SourceTimestamp, value.StatusCode);
+            }
+        }
+    }
+}
